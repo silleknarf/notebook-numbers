b7830c3e7cf4471d7daaca14996a2c25da0eae27
Frank Ellis <silleknarf@gmail.com>
2015-09-03 20:33:21 +0100

    Resolved merge conflicts from weird line numbers

--- game/config.js
+++ game/config.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿/**
+/**
  * @module NotebookNumbers
  **/
 (function () {
@@ -16,22 +15,3 @@
     };
     window.config = config;
 })();
-=======
-﻿/**
- * @module NotebookNumbers
- **/
-(function () {
-    var config = {
-        navy: "#003266",
-        font: "Londrina Solid",
-        //this.font = "Annie Use Your Telescope"
-        width: 700,
-        numColumns: 9,
-        height: 3000,
-        cellHeight: 40,
-        marginLeft: 15,
-        marginTop: 15,
-    };
-    window.config = config;
-})();
->>>>>>> 127b9bab58ee885d67b9cd25fa162e33683b29a1
--- game/event_manager.js
+++ game/event_manager.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿/**
+/**
  * @module NotebookNumbers
  **/
 (function () {
@@ -13,19 +12,3 @@
     eventManager.vent = Backbone.Events;
     window.eventManager = eventManager;
 })();
-=======
-﻿/**
- * @module NotebookNumbers
- **/
-(function () {
-    /**
-     * Add Backbone.js events
-     *
-     * @method vent
-     * @extends Backbone.Events
-     **/
-    var eventManager = {};
-    eventManager.vent = Backbone.Events;
-    window.eventManager = eventManager;
-})();
->>>>>>> 127b9bab58ee885d67b9cd25fa162e33683b29a1
