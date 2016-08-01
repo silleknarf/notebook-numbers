a760c2bf91cb223f9444895bd8066763d2e7b96f
Frank Ellis <silleknarf@gmail.com>
2016-08-01 22:53:38 +0100

    Fixed conflicts

--- game/notebook_numbers.js
+++ game/notebook_numbers.js
@@ -1,6 +1,5 @@
 var ecs = entityComponentSystem();
 
-<<<<<<< HEAD
 renderSystem(ecs, eventManager, preloaderMixin);
 boundsSystem(ecs, eventManager)
 
@@ -12,14 +11,3 @@ var init = function() {
 	eventManager.vent.trigger("SYSTEM:BOUNDS:START");
 	eventManager.vent.trigger("SYSTEM:RENDER:START");
 };
-=======
-renderSystem(ecs, eventManager, preloader);
-boundsSystem(ecs, eventManager)
-
-var backgroundEntityComponents = [backgroundViewComponent()];
-var backgroundEntity = entity(backgroundEntityComponents)
-ecs.entities.push(backgroundEntity);
-
-eventManager.vent.trigger("SYSTEM:BOUNDS:START");
-eventManager.vent.trigger("SYSTEM:RENDER:START");
->>>>>>> fe8d1f25a422bf780a9a5e28c7513ef65933b0fa
